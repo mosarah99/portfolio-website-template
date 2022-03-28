afc6c546fc94a11dcd861461af710b3008ada557
mosarah99 <m_rahman136593@fanshaweonline.ca>
2022-03-27 22:38:50 -0600

    added jquery of scrollheader
    Added jquery file
    Translated vanilla js to jquery of scrollheader function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
+/*=============== JQUERY ===============*/
+$(function() {  // jquery
+    /*=============== CHANGE BACKGROUND HEADER ===============*/
+    window.addEventListener('scroll', () => {
+        if(window.scrollY >= 50)
+            $("header").addClass('scroll-header');
+        else
+            $("header").removeClass('scroll-header');
+    });
+
+}); //jquery
 
 
 /*=============== SERVICES MODAL ===============*/
