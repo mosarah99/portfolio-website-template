9641094182e2107c7a786d600e3874fd23e73311
mosarah99 <m_rahman136593@fanshaweonline.ca>
2022-03-28 16:15:27 -0600

    updated entire js
    Converted all vanilla js code to jquery code.
    These sections are newly converted:
    - SCROLL SECTIONS ACTIVE LINK
    - LIGHT DARK THEME
    - SCROLL REVEAL ANIMATION

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*=============== JQUERY ===============*/
 $(function() {  // jquery
     /*=============== CHANGE BACKGROUND HEADER ===============*/
-    window.addEventListener('scroll', async () => {
+    $(window).scroll( async () => {
         if(window.scrollY >= 50)
             $("#header").addClass('scroll-header');
         else
@@ -60,95 +60,119 @@ $(function() {  // jquery
 
 
     /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-    // $("section[id]");
+    $(window).scroll( async () => {
+        const scrollY = $(window).scrollTop();
 
-    // window.addEventListener('scroll', async () => {
-    //     const scrollY = window.pageYOffset
-
-    //     $("section[id]").each(async (current) => {
-    //         const sectionHeight = current.offsetHeight,
-    //               sectionTop = current.offsetTop - 58,
-    //               sectionId = current.getAttribute('id');
+        $("section").each(async (idx) => {
+            let current = $("section")[idx];
+            const sectionHeight = current.offsetHeight,
+                  sectionTop = current.offsetTop - 58,
+                  sectionId = current.attributes.id.nodeValue;
 
 
-    //         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-    //             $('.nav__menu a[href*=' + sectionId + ']').addClass('active-link');
-    //         }else{
-    //             $('.nav__menu a[href*=' + sectionId + ']').removeClass('active-link');
-    //         }
-            
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                $('.nav__menu a[href*=' + sectionId + ']').addClass('active-link');
+                return false;
+            } else {
+                $('.nav__menu a[href*=' + sectionId + ']').removeClass('active-link');
+            }
+        });
+    });
 
-    //     });
-    // });
 
+    /*=============== LIGHT DARK THEME ===============*/ 
+    const toggleTheme = async () => {
+        let currentTheme = $("body").hasClass("light-theme") ? 'light':'dark'; // determine current theme
+        
+        // enable light-theme
+        if (currentTheme === 'dark') {
+            // Add the light / icon theme
+            $("body").addClass("light-theme");
+            $("#theme-button").addClass("bxs-sun");
+        } else {
+            // Remove the light / icon theme
+            $("body").removeClass("light-theme");
+            $("#theme-button").removeClass("bxs-sun");
+        }
+    };
+
+    // when the theme change button is pressed
+    $("#theme-button").click( async () => {
+        let currentTheme = $("body").hasClass("light-theme") ? 'light':'dark'; // determine current theme
+        
+        // enable light-theme
+        if (currentTheme === 'dark') {
+            // Add the light / icon theme
+            $("body").addClass("light-theme");
+            $("#theme-button").addClass("bxs-sun");
+        } else { // disable light theme
+            // Remove the light / icon theme
+            $("body").removeClass("light-theme");
+            $("#theme-button").removeClass("bxs-sun");
+        }
+    }); // toggle theme when theme button pressed
+
+
+    /*=============== SCROLL REVEAL ANIMATION ===============*/
+    const sr = ScrollReveal ({
+        origin: 'top',
+        distance: '60px',
+        duration: 2500,
+        delay: 400,
+        //reset: true,
+    })
+    
+    sr.reveal(`.home__data`);
+    sr.reveal(`.home__handle`, {delay: 700});
+    sr.reveal(`.home__social, .home__scroll`, {delay: 900, origin: 'bottom'});
+    
+    
+    
 
 }); //jquery
 
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-// const sections = document.querySelectorAll('section[id]')
-
-// function scrollActive(){
-//     const scrollY = window.pageYOffset
-
-//     sections.forEach(current =>{
-//         const sectionHeight = current.offsetHeight,
-//               sectionTop = current.offsetTop - 58,
-//               sectionId = current.getAttribute('id')
-
-//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-//         }else{
-//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-//         }
-//     })
-// }
-// window.addEventListener('scroll', scrollActive)
-
-
 /*=============== LIGHT DARK THEME ===============*/ 
-const themeButton = document.getElementById('theme-button')
-const lightTheme = 'light-theme'
-const iconTheme = 'bxs-sun'
+// const themeButton = document.getElementById('theme-button')
+// const lightTheme = 'light-theme'
+// const iconTheme = 'bxs-sun'
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+// // Previously selected topic (if user selected)
+// const selectedTheme = localStorage.getItem('selected-theme')
+// const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the light-theme class
-const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bxs-moon' : 'bx bxs-sun'
+// // We obtain the current theme that the interface has by validating the light-theme class
+// const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bxs-moon' : 'bx bxs-sun'
 
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
-  themeButton.classList[selectedIcon === 'bx bxs-moon' ? 'add' : 'remove'](iconTheme)
-}
+// // We validate if the user previously chose a topic
+// if (selectedTheme) {
+//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+//   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+//   themeButton.classList[selectedIcon === 'bx bxs-moon' ? 'add' : 'remove'](iconTheme)
+// }
 
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the light / icon theme
-    document.body.classList.toggle(lightTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
+// // Activate / deactivate the theme manually with the button
+// themeButton.addEventListener('click', () => {
+//     // Add or remove the light / icon theme
+//     document.body.classList.toggle(lightTheme)
+//     themeButton.classList.toggle(iconTheme)
+//     // We save the theme and the current icon that the user chose
+//     localStorage.setItem('selected-theme', getCurrentTheme())
+//     localStorage.setItem('selected-icon', getCurrentIcon())
+// })
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal ({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-    //reset: true,
-})
+// const sr = ScrollReveal ({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 400,
+//     //reset: true,
+// })
 
-sr.reveal(`.home__data`)
-sr.reveal(`.home__handle`, {delay: 700})
-sr.reveal(`.home__social, .home__scroll`, {delay: 900, origin: 'bottom'})
+// sr.reveal(`.home__data`)
+// sr.reveal(`.home__handle`, {delay: 700})
+// sr.reveal(`.home__social, .home__scroll`, {delay: 900, origin: 'bottom'})
 
 
