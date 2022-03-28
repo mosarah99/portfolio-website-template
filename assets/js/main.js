3bf0e0902f58567b5b13bfaa9be82e20d410ff44
mosarah99 <m_rahman136593@fanshaweonline.ca>
2022-03-28 16:19:09 -0600

    comments removed
    Unnecessary comment block removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,9 +126,6 @@ $(function() {  // jquery
     sr.reveal(`.home__handle`, {delay: 700});
     sr.reveal(`.home__social, .home__scroll`, {delay: 900, origin: 'bottom'});
     
-    
-    
-
 }); //jquery
 
 /*=============== LIGHT DARK THEME ===============*/ 
@@ -160,19 +157,3 @@ $(function() {  // jquery
 //     localStorage.setItem('selected-theme', getCurrentTheme())
 //     localStorage.setItem('selected-icon', getCurrentIcon())
 // })
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-// const sr = ScrollReveal ({
-//     origin: 'top',
-//     distance: '60px',
-//     duration: 2500,
-//     delay: 400,
-//     //reset: true,
-// })
-
-// sr.reveal(`.home__data`)
-// sr.reveal(`.home__handle`, {delay: 700})
-// sr.reveal(`.home__social, .home__scroll`, {delay: 900, origin: 'bottom'})
-
-
